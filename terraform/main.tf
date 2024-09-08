b78a8888fc340119076a89fce12dcf11b00cb0a1
Kevin Wang <k499wang@uwaterloo.ca>
2024-09-08 19:53:03 -0400

    Updated Domain name

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,31 +6,42 @@ terraform {
     }
   }
 }
+# domain
+resource "digitalocean_domain" "domain"{
+  name = "k3vinwvng.com"
+}
 # ssh keys
-resource "digitalocean_ssh_key" "default" {
-  name       = "Terraform Example"
+resource "digitalocean_ssh_key" "k3vinwvng" {
+  name       = "Terraform k3vinwvng"
   public_key = file("${path.module}/files/id_rsa.pub")
+}
+
+resource "digitalocean_record" "default" {
+  domain = digitalocean_domain.domain.name
+  type   = "A"
+  name   = "@"
+  value  =  digitalocean_droplet.k3vinwvng.ipv4_address
 }
 
 
 provider "digitalocean" {
 }
 
-resource "digitalocean_droplet" "example" {
-  name   = "example-droplet"
+resource "digitalocean_droplet" "k3vinwvng" {
+  name   = "k3vinwvng-droplet"
   size   = "s-1vcpu-1gb"
   image  = "ubuntu-20-04-x64"
   region = "nyc1"
   tags   = ["web"]
   monitoring = true
-  private_networking = true
+
   ssh_keys = [
-    digitalocean_ssh_key.example.id
+    digitalocean_ssh_key.k3vinwvng.id
   ]
   user_data = file("${path.module}/files/user-data.sh")
 }
 
 output "droplet_ip" {
-  value = digitalocean_droplet.example.ipv4_address
+  value = digitalocean_droplet.k3vinwvng.ipv4_address
 }
 
